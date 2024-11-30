bb65f215b0564413cb2014401857729b585bdef5
overwell24 <overwell24@gmail.com>
2024-11-30 22:32:47 +0900

    add: project github link

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,56 +3,56 @@ export const projects = [
       title: "딸깍",
       description: "생성형 AI(LLM)을 활용한 유튜브 영상 요약 서비스",
       tags: [ "Flask", "LangChain", "OpenAI"],
-      link: "https://www.naver.com/",
+      link: "https://github.com/overwell24/ttalkkag",
       image: "ttalkkag2.png"
     },
     {
       title: "육안(肉眼)",
       description: "소고기 등심 등급 분류 모델을 이용한 소고기 등심 등급 예측 서비스",
       tags: ["Flask", "PyTorch", "resnext50_32x3d"],
-      link: "#",
+      link: "https://github.com/overwell24/yugan",
       image: "yugan2.png"
     },
     {
       title: "자리 있어?",
       description: "실시간 카페 좌석 현황 서비스",
-      tags: ["Flask", "React", "Arduino"],
-      link: "#",
+      tags: ["Flask", "React", "Arduino", "AWS"],
+      link: "https://github.com/overwell24/wireless-network",
       image: "cafe.png"
     },
     {
       title: "빅데이터 분석 프로젝트",
       description: "빅데이터 분석을 통한 한전 적자 원인 분석",
       tags: ["Pandas", "Seaborn", "Matplotlib"],
-      link: "#",
+      link: "https://github.com/overwell24/bigdata-report",
       image: "big_data.png"
     },
     {
       title: "인하공전 과제 알림이",
       description: "인하공전 과제 푸쉬 알람 서비스",
-      tags: ["Flask", "Selenium", "TelegramAPI"],
-      link: "#",
+      tags: ["Flask", "Selenium", "TelegramAPI", "AWS"],
+      link: "https://github.com/overwell24/assignment-reminder",
       image: "reminder.jpeg"
     },
     {
       title: "Spring 프로젝트",
       description: "spring 게시판",
       tags: ["Spring", "MySQL"],
-      link: "#",
+      link: "https://github.com/overwell24/spring-study",
       image: "spring_study.png"
     },
     {
       title: "Initial Quiz",
       description: "초성퀴즈 게임",
       tags: ["Java", "SWING", "MySQL"],
-      link: "#",
+      link: "https://github.com/overwell24/initial-quiz",
       image: "initial_quiz.png"
     },
     {
       title: "Bookmark Taager",
       description: "북마크 태그 저장 서비스",
       tags: ["JSP", "MySQL"],
-      link: "#",
+      link: "https://github.com/overwell24/bookmark-tagger",
       image: "bookmark_tagger.png"
     }
   ];
